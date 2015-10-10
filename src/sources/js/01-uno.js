af35f3fbbff79e86d2f38373a1ca6f3eb09fdcf1
Javier Sanchez Toledano <js.toledano@me.com>
2015-10-10 16:35:16 -0500

    Prueba de watch

--- src/sources/js/01-uno.js
+++ src/sources/js/01-uno.js
@@ -3,4 +3,4 @@ month = test.getMonth();
 month = (month * 1) + 1;
 day = test.getDate();
 year = test.getFullYear();
-console.log(" ",month,"/",day,"/",year," ");
+console.log("(",month,"/",day,"/",year,")");
