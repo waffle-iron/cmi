45df5f544f069f2162398ee7cedd8bbb39bfd3b5
Javier Sanchez Toledano <js.toledano@me.com>
2015-10-19 15:50:03 -0500

    Se copypastea el modelo de la pagina
    https://thinkster.io/django-angularjs-tutorial

---
+++ src/core/models.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+
+#         app: mx.ine.sgc.cmi
+#      módulo: core.models
+# descripción: Modelos básicos del núcleo del CMI
+#  __author__: Javier Sanchez Toledano
+#       fecha: oct 19, 2015
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
+
+
+
