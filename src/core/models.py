eaec1a5e8a8313456846c005581efbbd51068b1a
Javier Sanchez Toledano <js.toledano@me.com>
2015-10-19 20:50:27 -0500

    Compaibilidad con Python2

--- src/core/models.py
+++ src/core/models.py
@@ -8,8 +8,11 @@
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Pipol(AbstractBaseUser):
     '''
     Clase para gestionar los usuarios del cuadro de mando integral.
@@ -81,7 +84,7 @@ class Pipol(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
-    def __unicode__(self):
+    def __str__(self):
         return self.email
 
     def get_full_name(self):
