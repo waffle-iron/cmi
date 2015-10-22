b2bc25fec3330f2634939f7d97f526cba303824e
Javier Sanchez Toledano <js.toledano@me.com>
2015-10-22 10:35:42 -0500

    Clases para administrar Pipol

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,10 +1,25 @@
 # -*- coding: UTF-8 -*-
 
-#         app: 
-#      módulo: 
-# descripción: 
+#         app: mx.ine.tlax.cmi
+#      módulo: core.admin
+# descripción: Administración de pipol
 #  __author__: Javier Sanchez Toledano
-#       fecha: 
+#       fecha: octubre 22 de 2015
+
+from django.contrib import admin
+from django.utils.translation import gettext as _
+from django.contrib.auth.admin import UserAdmin
+from .models import Pipol
 
 
+class PipolAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': [('email', 'username', 'password')]}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('INE'), {'fields': ('entidad', 'sitio', 'puesto', )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
+
+admin.site.register(Pipol, PipolAdmin)
