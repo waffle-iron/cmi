d9dd4560138499f3da421d88dfcc3ab5fb9cb327
Javier Sanchez Toledano <js.toledano@me.com>
2015-10-24 00:15:28 -0500

    Vistas de REST API.

---
+++ src/core/view.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+
+#         app: mx.ine.sgc.cmi
+#      módulo: core.models
+# descripción: Modelos básicos del núcleo del CMI
+#  __author__: toledano
+#       fecha: oct 24, 2015
+
+from .models import Pipol
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import PipolSerializer, GroupSerializer
+
+
+class PipolViewSet(viewsets.ModelViewSet):
+    """
+    Punto de acceso API que permite ver y editar usuarios.
+    """
+    queryset = Pipol.objects.all().order_by('-date_joined')
+    serializer_class = PipolSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Punto de acceso API que permite ver y editar grupos.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
