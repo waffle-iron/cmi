a54b95b6fed27717e1b9137f43b0264a38ac7dc1
Javier Sanchez Toledano <js.toledano@me.com>
2015-11-07 10:14:03 -0600

    Ajustes de PyLint TG-23 #working

--- src/core/views.py
+++ src/core/views.py
@@ -6,9 +6,9 @@
 #  __author__: toledano
 #       fecha: oct 24, 2015
 
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import TemplateView
-from django.utils.decorators import method_decorator
 
 from rest_framework import permissions, viewsets
 
