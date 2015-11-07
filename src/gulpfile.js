ee076f6fe14d0c1698321f17795b72438a5cc018
Javier Sanchez Toledano <js.toledano@me.com>
2015-11-06 23:51:14 -0600

    Una nueva tarea.
    TG-23 La tarea `scripts`

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,10 +7,17 @@ var gulp = require('gulp');
 
 // incluye los plugin
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var comments = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
 
-// Se define la tarea `jshint`
-gulp.task('jshint', function() {
-  gulp.src('./sources/js/*.js')
+// Se define la tarea `scripts`
+gulp.task('scripts', function() {
+  gulp.src(['./sources/js/*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('script.js'))
+    .pipe(comments())
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/js/'));
 });
