b0f859521f1cb48a67024d589ee7ea28fd698f86
Javier Sanchez Toledano <js.toledano@me.com>
2015-11-06 21:54:15 -0600

    Correcciones por PyLint

--- src/core/views.py
+++ src/core/views.py
@@ -37,7 +37,10 @@ class PipolViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             Pipol.objects.create_user(**serializer.validated_data)
 
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_201_CREATED
+            )
 
         return Response({
             'status': 'Bad request',
