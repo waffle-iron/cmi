f06c0ce4a74171bee08d71c5e97dd1182ba322dc
Javier Sanchez Toledano <js.toledano@me.com>
2016-04-12 15:36:02 -0500

    Limpiando las proquerías que hice

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url, patterns
-# from django.contrib import admin
+from django.contrib import admin
 
 from rest_framework_nested import routers
 from .views import PipolViewSet, Index, UserViewSets
@@ -8,24 +8,10 @@ router = routers.SimpleRouter()
 router.register(r'pipols', PipolViewSet)
 router.register(r'users', UserViewSets)
 
-<<<<<<< HEAD
 urlpatterns = [
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-=======
-urlpatterns = patterns(
-    '',
->>>>>>> feature/authentication.service
+    url(r'^api-auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/', include(router.urls)),
     url(r'^.*$', Index.as_view(), name='index'),
-)
-
-
-#     [
-#
-#     url(r'^admin/', include(admin.site.urls)),
-#     url(r'^api/v1/', include(router.urls)),
-#
-#     url(r'^.*$', Index.as_view(), name='index'),
-# ]
+]
