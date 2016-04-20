3335ded12f3f75cbacd930c503e073d65fa5e964
Javier Sanchez Toledano <js.toledano@me.com>
2016-04-20 16:07:23 -0500

    Primer intento de uso de $resource
    Caso #20

--- src/sources/js/CMIapp/core.controllers.js
+++ src/sources/js/CMIapp/core.controllers.js
@@ -1,5 +1,6 @@
 angular.module('core.controllers', [])
-  .controller('PoliticController', PoliticController);
+  .controller('PoliticController', PoliticController)
+  .factory("PoliticsFactory", PoliticsFactory);
 
 // PoliticController.$inject = ['$scope', '$http'];
 
@@ -8,4 +9,12 @@ function PoliticController($scope, $http) {
     .success(function (data) {
       $scope.politica = data;
   });
+}
+
+function PoliticsFactory($resource) {
+  return $resource(
+    "http://localhost:8000/api/v1.0/politica/?format=json",
+    {},
+    {get: {method: "GET", isArray: true}}
+  )
 }
