a5cb9f29f4f0d38b5dbd30d910525a6d9c3eb63b
Javier Sanchez Toledano <js.toledano@me.com>
2016-04-27 14:29:23 -0500

    Caso #30 - cmi - Crear login con DRF y Angular
    Se agrega el patrón de búsqueda para /login/

--- src/core/urls.py
+++ src/core/urls.py
@@ -8,6 +8,7 @@ from .views import (
     UserViewSets,
     PoliticaActual,
     PoliticaViewSet,
+    LoginView,
     actual
 )
 
@@ -19,8 +20,8 @@ router.register(r'v1.0/actual', PoliticaActual, base_name='actual')
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include(
-        'rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/v1.0/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1.0/actual/', actual, name='actual'),
     url(r'^api/', include(router.urls)),
     url(r'^$', Index.as_view(), name='index'),
