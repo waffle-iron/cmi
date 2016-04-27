d4f1d52f2ba2a9cb51ffbc1abfdec211a73aa684
Javier Sanchez Toledano <js.toledano@me.com>
2016-04-27 11:47:45 -0500

    Caso #30 - cmi - Crear login con DRF y Angular
    Se corrige la función AuthenticationFactory.

--- src/core/assets/core/js/core.controllers.js
+++ src/core/assets/core/js/core.controllers.js
@@ -52,7 +52,7 @@
     var vm = this;
     vm.register = register;
     function register() {
-      Authentication.register(vm.email, vm.password, vm.username);
+      AuthenticationFactory.register(vm.email, vm.password, vm.username);
     }
   }
 
